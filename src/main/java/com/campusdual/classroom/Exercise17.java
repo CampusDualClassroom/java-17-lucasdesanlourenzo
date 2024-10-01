4e2ef8aa9ac4c10037d30179b7241750d147959f
lucasdesanlourenzo <luqueras2000@gmail.com>
2024-10-01 09:49:33 +0200

    Ejercicio 17 java

--- src/main/java/com/campusdual/classroom/Exercise17.java
+++ src/main/java/com/campusdual/classroom/Exercise17.java
@@ -1,19 +1,44 @@
 package com.campusdual.classroom;
 
 public class Exercise17 {
-	public static int[] createEmptyIntArray(int positions) {}
+	public static int[] createEmptyIntArray(int positions) {
+		return new int[positions];
+	}
 
-	public static String[] createInitializedStringArray() {}
+	public static String[] createInitializedStringArray() {
 
-	public static void populateArray(int[] array, int index, int value) {}
 
-	public static int arrayLength(int[] array) {}
+		String[] array = new String[3];
+		array[0] = "1";
+		array[1] = "2";
+		array[2] = "3";
+		return  array;
+	}
 
-	public static void showValue(int[] array, int index) {}
+	public static void populateArray(int[] array, int index, int value) {
+		if(array.length > index -1 ){
+			array[index] = value;
+		}else{
+			System.out.println("No existe esa posicion");
+		}
+	}
+
+	public static int arrayLength(int[] array) {
+		return  array.length;
+	}
+
+	public static void showValue(int[] array, int index) {
+
+		if(array.length > index -1 ){
+			System.out.println(array[index]);
+		}else{
+			System.out.println("No existe esa posicion");
+		}
+	}
 
 	public static void main(String[] args) {
 		int[] intArray = createEmptyIntArray(3);
-        String[] stringArray = createInitializedStringArray();
+		String[] stringArray = createInitializedStringArray();
 		populateArray(intArray, 0, 10);
 		populateArray(intArray, 1, 9);
 		populateArray(intArray, 2, 8);
